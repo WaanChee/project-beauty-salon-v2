336c61f77e886d61b5403b2c305f3bf4539fda28
Chan <waanchee@gmail.com>
2026-01-10 21:52:34 +0800

    config: update API endpoint

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -16,7 +16,7 @@ import pic2 from "../assets/images/beauty-salon-login-page-bg.png";
 export default function Login() {
   const loginImage = pic2;
   const API_URL =
-    "https://eb392aa9-9b22-4ac4-b3cc-7f8d369efa1e-00-3asbq4cs361u6.pike.replit.dev";
+    "https://bdf8b629-3ab0-47ff-9325-50227345e965-00-1zki6gacehg14.pike.replit.dev";
 
   // Possible values: null (no modal shows), "Login", "SignUp"
   const [modalShow, setModalShow] = useState(null);
