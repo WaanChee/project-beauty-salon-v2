8dd32335194a7a0ceedd67641b5c845546bf4a76
Chan <waanchee@gmail.com>
2026-01-10 22:02:57 +0800

    feat: add customer reducer to Redux store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bookingReducer from "./features/bookings/bookingSlice";
+import customerReducer from "./features/customers/customerSlice";
 
 // Create the Redux store
 const store = configureStore({
   reducer: {
     bookings: bookingReducer,
+    customers: customerReducer,
   },
 });
 
