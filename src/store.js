ca2af5906f78c1940204f996d78d2264df48e85e
Chan <waanchee@gmail.com>
2026-01-17 00:04:37 +0800

    feat: add gallery reducer to Redux store configuration

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bookingReducer from "./features/bookings/bookingSlice";
 import customerReducer from "./features/customers/customerSlice";
+import galleryReducer from "./features/gallery/gallerySlice";
 
 // Create the Redux store
 const store = configureStore({
   reducer: {
     bookings: bookingReducer,
     customer: customerReducer,
+    gallery: galleryReducer,
   },
 });
 
